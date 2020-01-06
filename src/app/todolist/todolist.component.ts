31bddfcf43d6e731dd4ae3e19752a3d45d9fb38f
mdzobayer <mdzobayer@rocketmail.com>
2020-01-06 11:26:38 +0600

    Add edit option but style broken :)

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+class Task {
+  status:boolean; editable:boolean; description:string;
+  constructor(status:boolean, editable:boolean, description:string) {
+    this.status = status;
+    this.editable = editable;
+    this.description = description;
+  }
+}
+
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit {
-  myTask: string[] = ["BBQ Party", "Adda with friends", "Tour plans"];
+  myTask: Task[] = [];
   newTask: string;
 
-  constructor() { }
+  constructor() {
+    this.myTask.push({
+      status: false,
+      editable: false,
+      description: "BBQ Party"
+    });
+    this.myTask.push({
+      status: false,
+      editable: false,
+      description: "Adda with friends"
+    });
+    this.myTask.push({
+      status: false,
+      editable: false,
+      description: "Tour plans"
+    });
+  }
 
   ngOnInit() {
   }
@@ -17,7 +42,10 @@ export class TodolistComponent implements OnInit {
   onKeyUp() {
     this.newTask = this.newTask.trim();
     if (this.newTask.length > 0) {
-      this.myTask.push(this.newTask);
+      this.myTask.push({
+        status: false,
+        editable: false,
+        description: this.newTask});
       console.log("New Task is: " + this.newTask);
     }
     this.newTask = "";
@@ -26,7 +54,10 @@ export class TodolistComponent implements OnInit {
   addTask() {
     this.newTask = this.newTask.trim();
     if (this.newTask.length > 0) {
-      this.myTask.push(this.newTask);
+      this.myTask.push({
+        status: false,
+        editable: false,
+        description: this.newTask});
       console.log("New Task is: " + this.newTask);
     }
     this.newTask = "";
@@ -34,6 +65,16 @@ export class TodolistComponent implements OnInit {
   removeTask(aTask) {
     let index = this.myTask.indexOf(aTask);
     this.myTask.splice(index, 1);
+  }
+
+  editTask(aTask) {
+    let index = this.myTask.indexOf(aTask);
+    this.myTask[index].editable = true;
+  }
+
+  doneEditTask(aTask) {
+    let index = this.myTask.indexOf(aTask);
+    this.myTask[index].editable = false;
   }
 
 }
